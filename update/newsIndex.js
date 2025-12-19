8546f7fbfd06c9df3388fdd999505775f1b69619
unknown <web230529@gmail.com>
2025-12-19 11:44:30 +0900

    HP配下を新しい内容に差し替え

--- update/newsIndex.js
+++ update/newsIndex.js
@@ -4,8 +4,15 @@ const newsList = [
     date: "2025/10/30",
     title: "出口青空が若手講演奨励賞を受賞しました！",
     content: "",
-    url: "./update/award/2025/ZairyotoKankyotoronkai251114deguchi/index.html"  // ← ある場合だけリンク
+    url: "./update/award/2025/zairyo-kankyo-toronkai-competition-2025-11-14-deguchi/"  // ← ある場合だけリンク
   },
+  {
+    date: "2025/09/05",
+    title: "山口雄也が優秀論文発表者賞を受賞しました！",
+    content: "",
+    url: "./update/award/2025/jiban-kogaku-kenkyu-happyokai-2025-09-05-yamaguchi/"  
+  },
+
 
 
 
@@ -15,7 +22,7 @@ const newsList = [
 const membersList = [
   {
     year: "2025",
-    url: "./2025/index.html"  // ← ある場合だけリンク
+    url: "./2025/"  // ← ある場合だけリンク
   },
 
 ];
