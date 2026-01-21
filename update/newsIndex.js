99ec079b151b9a4750724c154c4a9fc99fe0e8c9
unknown <web230529@gmail.com>
2026-01-21 20:12:36 +0900

    修正を反映

--- update/newsIndex.js
+++ update/newsIndex.js
@@ -1,6 +1,12 @@
 // ニュース追加時更新
 const newsList = [
   {
+    date: "2026/01/13",
+    title: "出口青空が、2025年度JCI研究助成に採択されました！",
+    content: "",
+    url: "./update/award/2026/jci-kenkyu-josei-2026-01-13-deguchi/" , // ← ある場合だけリンク
+  },
+  {
     date: "2025/10/30",
     title: "出口青空が若手講演奨励賞を受賞しました！",
     content: "",
@@ -10,7 +16,7 @@ const newsList = [
     date: "2025/09/05",
     title: "山口雄也が優秀論文発表者賞を受賞しました！",
     content: "",
-    url: "./update/award/2025/jiban-kogaku-kenkyu-happyokai-2025-09-05-yamaguchi/"  
+    url: "./update/award/2025/jiban-kogaku-kenkyu-happyokai-2025-09-05-yamaguchi/"
   },
 
 
