2f49b4072a9e01d4289d434dff39bbdc48d3f8c3
hashimotonlab <web230529@gmail.com>
2026-06-17 20:44:34 +0900

    出口君吉田奨励賞受賞

--- update/newsIndex.js
+++ update/newsIndex.js
@@ -1,27 +1,34 @@
 // ニュース追加時更新
 const newsList = [
+    {
+    date: "2026/06/17",
+    title: "出口青空が、吉田研究奨励賞を受賞しました。",
+    content: "",
+    url: "./update/award/2026/YoshidaAward-2026-06-17-deguchi/", // ← ある場合だけリンク
+  }
+  ,
   {
     date: "2026/04/30",
-    title: "春のオープンキャンパスの写真追加しました",
+    title: "春のオープンキャンパスの写真追加しました。",
     content: "",
     url: "./update/event/2026/20260419spring_oc/", // ← ある場合だけリンク
   }
   ,
   {
     date: "2026/01/13",
-    title: "出口青空が、2025年度JCI研究助成に採択されました！",
+    title: "出口青空が、2025年度JCI研究助成に採択されました。",
     content: "",
     url: "./update/award/2026/jci-kenkyu-josei-2026-01-13-deguchi/", // ← ある場合だけリンク
   },
   {
     date: "2025/10/30",
-    title: "出口青空が若手講演奨励賞を受賞しました！",
+    title: "出口青空が若手講演奨励賞を受賞しました。",
     content: "",
     url: "./update/award/2025/zairyo-kankyo-toronkai-competition-2025-11-14-deguchi/"  // ← ある場合だけリンク
   },
   {
     date: "2025/09/05",
-    title: "山口雄也が優秀論文発表者賞を受賞しました！",
+    title: "山口雄也が優秀論文発表者賞を受賞しました。",
     content: "",
     url: "./update/award/2025/jiban-kogaku-kenkyu-happyokai-2025-09-05-yamaguchi/"
   },
