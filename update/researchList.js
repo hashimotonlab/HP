24f76d7c360da00b26e5eed37559dec9aa049c0c
hashimotonlab <web230529@gmail.com>
2026-07-05 23:46:25 +0900

    transient-response add

--- update/researchList.js
+++ update/researchList.js
@@ -1,10 +1,5 @@
 const researchList = [
-    // {
-    //     date: "2026/07/08",
-    //     title: "過渡応答解析で鉄筋の分極抵抗を測定する",
-    //     // content: "2026/04/19に開催した春のオープンキャンパスの写真を追加しました。",
-    //     url: "https://transient-response.onrender.com/"
-    // },
+
     // {
     //     date: "2026/07/08",
     //     title: "セメント硬化体中pHのin-situ測定",
@@ -13,8 +8,12 @@ const researchList = [
     // },
     {
         date: "2026/07/05",
+        title: "過渡応答解析で鉄筋の分極抵抗を測定する",
+        url: "https://transient-response.onrender.com/"
+    },
+    {
+        date: "2026/07/05",
         title: "見たまま組めばできるポテンショスタット",
-        // content: "2026/04/19に開催した春のオープンキャンパスの写真を追加しました。",
         url: "https://hashimotonlab.github.io/potentiostat/"
     },
 
