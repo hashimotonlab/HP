40881a97f8150e3dac0060f7d4d9106ffa7ca24f
hashimotonlab <web230529@gmail.com>
2026-07-06 22:50:08 +0900

    セメント硬化体中pHのin-situ測定追加

--- update/researchList.js
+++ update/researchList.js
@@ -1,11 +1,11 @@
 const researchList = [
 
-    // {
-    //     date: "2026/07/08",
-    //     title: "セメント硬化体中pHのin-situ測定",
-    //     // content: "2026/04/19に開催した春のオープンキャンパスの写真を追加しました。",
-    //     url: "https://ph-analysis-wwmu.onrender.com/"
-    // },
+    {
+        date: "2026/07/06",
+        title: "セメント硬化体中pHのin-situ測定",
+        // content: "2026/04/19に開催した春のオープンキャンパスの写真を追加しました。",
+        url: "https://ph-analysis-wwmu.onrender.com/"
+    },
     {
         date: "2026/07/05",
         title: "過渡応答解析で鉄筋の分極抵抗を測定する",
